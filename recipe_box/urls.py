ddbd7dd155102a01b10fa8debf7fa0e56ea90e32
Rob Spears <rob.sp3ars@gmail.com>
2020-04-29 19:36:29 -0400

    Append recipes app url routes to server routes

--- recipe_box/urls.py
+++ recipe_box/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.urls import urlpatterns as recipe_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += recipe_urls
