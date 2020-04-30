8debd535ce9d5856d20cc32b2ab81d32b746929e
Rob Spears <rob.sp3ars@gmail.com>
2020-04-30 10:09:27 -0400

    Config chef page path to accept chef id slug

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from recipes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('chef/<name>', views.chef, name='chef'),
-    path('recipe/<recipe_id>', views.recipe, name='recipe')
+    path('chefs/<chef_id>', views.chefs, name='chefs'),
+    path('recipes/<recipe_id>', views.recipes, name='recipes')
 ]
