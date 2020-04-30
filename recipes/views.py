a7c87b69e899463d9ef2c0416f2c011550bc1910
Rob Spears <rob.sp3ars@gmail.com>
2020-04-29 20:47:20 -0400

    Pass recipe list data to index view

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 
 def index(request):
-    return render(request, 'index.html')
+    recipe_list = Recipe.objects.all()
+    return render(request,
+                  'index.html',
+                  {'recipe_list': recipe_list})
