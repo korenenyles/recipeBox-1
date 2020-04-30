05a863f9bba0bc06fb80040698a894e400ec1379
Rob Spears <rob.sp3ars@gmail.com>
2020-04-29 21:59:50 -0400

    Add url paths for individual chefs and recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,6 @@ from recipes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('chef/<name>', views.chef, name='chef'),
+    path('recipe/<recipe_id>', views.recipe, name='recipe')
 ]
