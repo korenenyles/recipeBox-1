25bd3a20f8a6408de3ff98234f707646dab8c3b2
Rob Spears <rob.sp3ars@gmail.com>
2020-04-29 22:00:25 -0400

    Configure views for pages about individual chefs and recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from recipes.models import Recipe
+from recipes.models import Recipe, Chef
 
 
 def index(request):
@@ -7,3 +7,17 @@ def index(request):
     return render(request,
                   'index.html',
                   {'recipe_list': recipe_list})
+
+
+def chef(request, name=None):
+    selected_chef = Chef.objects.get(name=name)
+    return render(request,
+                  'chef.html',
+                  {'chef': selected_chef})
+
+
+def recipe(request, recipe_id=None):
+    selected_recipe = Recipe.objects.get(id=recipe_id)
+    return render(request,
+                  'recipe.html',
+                  {'recipe': selected_recipe})
