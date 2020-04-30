4e0690a8f43bfd5be94ab4b8c4b34c365f0e867d
Rob Spears <rob.sp3ars@gmail.com>
2020-04-29 20:46:13 -0400

    Create Recipe and Chef models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chef(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField(max_length=30)
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
