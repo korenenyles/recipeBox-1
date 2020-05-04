2cc0ae6397ff293795bf4b304b4f6bf460a0e089
Rob Spears <rob.sp3ars@gmail.com>
2020-05-04 14:34:56 -0400

    add appropriate url paths to views that add recipes and chefs

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,8 @@ from recipes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addchef/', views.chef_add, name='new_chef'),
     path('chefs/<chef_id>', views.chefs, name='chefs'),
+    path('addrecipe/', views.recipe_add, name='new_recipe'),
     path('recipes/<recipe_id>', views.recipes, name='recipes')
 ]
