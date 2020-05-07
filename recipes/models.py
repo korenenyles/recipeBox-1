6b85b7f24b71420d38f11439f93b2746b9ec5e42
Rob Spears <rob.sp3ars@gmail.com>
2020-05-06 22:50:30 -0400

    Establish one-to-one relationship between Chefs and Users

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ from django.db import models
 class Chef(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField()
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
