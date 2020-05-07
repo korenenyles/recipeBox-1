24a0e6003eba8e7397a4d1afb493910fe0428462
Rob Spears <rob.sp3ars@gmail.com>
2020-05-06 22:51:14 -0400

    Add login and logout url paths

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('addchef/', views.chef_add, name='new_chef'),
     path('chefs/<chef_id>', views.chefs, name='chefs'),
     path('addrecipe/', views.recipe_add, name='new_recipe'),
-    path('recipes/<recipe_id>', views.recipes, name='recipes')
+    path('recipes/<recipe_id>', views.recipes, name='recipes'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout')
 ]
